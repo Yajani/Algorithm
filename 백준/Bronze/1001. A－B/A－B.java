e82a1da35a218583d8b18998a13c7a6ba024593d
예진초이 <115179558+Yajani@users.noreply.github.com>
2023-10-14 23:17:45 +0900

    [Bronze V] Title: A-B, Time: 128 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,20 @@
+//방법1 Scanner 사용하기
+import java.io.BufferedReader;
+import java .io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        //BufferedReader방식
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(a - b);
+
+    }
+}
