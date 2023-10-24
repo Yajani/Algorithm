5f4d9e8c0bb5e3beeb045bea988c6ba3bcb7d9c7
예진초이 <115179558+Yajani@users.noreply.github.com>
2023-10-25 00:04:48 +0900

    [level unrated] Title: 편지, Time: 0.01 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/unrated/120898. 편지/편지.java
+++ 프로그래머스/unrated/120898. 편지/편지.java
@@ -1,8 +1,11 @@
 class Solution {
     public int solution(String message) {
-        int answer = 0;
-        String[] list = message.split("");
-        answer = list.length * 2;
-        return answer;
+        // int answer = 0;
+        // String[] list = message.split("");
+        // answer = list.length * 2;
+        // return answer;
+        
+        return message.length()*2;
+        
     }
 }
