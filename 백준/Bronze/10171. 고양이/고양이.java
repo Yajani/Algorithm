fc54b0503ac0fd754970af7ba4e45adae0ef31b3
예진초이 <115179558+Yajani@users.noreply.github.com>
2023-11-17 22:10:47 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 13880 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,11 @@
+
+public class Main{
+    public static void main(String[] args) {
+        // \는 단독으로 출력이 불가능하다 -> \\으로 표기 
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+    }
+}
