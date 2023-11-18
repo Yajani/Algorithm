d5be80a639b7530063ff78a3f9d343aeef9be9f3
예진초이 <115179558+Yajani@users.noreply.github.com>
2023-11-18 23:12:19 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 13908 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,12 @@
+
+public class Main{
+    public static void main(String[] args) {
+        // \는 단독으로 출력이 불가능하다 -> \\으로 표기
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+
+    }
+}
