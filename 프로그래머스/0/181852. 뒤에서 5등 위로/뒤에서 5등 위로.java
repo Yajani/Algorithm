1268e33f75e36c64b13c9792d643b4cea294f82b
예진초이 <115179558+Yajani@users.noreply.github.com>
2024-02-05 00:42:57 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.54 ms, Memory: 68.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length - 5];
+        int sum = 5;
+        Arrays.sort(num_list);
+        
+        for(int i=0; i< answer.length; i++){
+            answer[i] = num_list[sum];
+            sum++;
+            
+        }
+        
+        return answer;
+    }
+}
