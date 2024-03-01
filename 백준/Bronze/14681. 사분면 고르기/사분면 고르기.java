4ee5e7f0cfc86e506bdf76cb1555b91a325746f6
예진초이 <115179558+Yajani@users.noreply.github.com>
2024-03-01 23:58:10 +0900

    [Bronze V] Title: 사분면 고르기, Time: 124 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,49 @@
+//BuferedReader 사용하기 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int x = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine());
+
+
+
+        if(x > 0 && y>0){
+            System.out.println("1");
+        } else if (x <0 && y >0) {
+            System.out.println("2");
+
+        } else if (x<0 && y <0) {
+            System.out.println("3");
+
+        } else{
+            System.out.println("4");
+        }
+        
+//        if(x>0){
+//            if(y>0){
+//                System.out.println(1);
+//            }
+//            else {
+//                System.out.println(4);
+//            }
+//        }
+//        else {
+//            if(y>0){
+//                System.out.println(2);
+//            }else{
+//                System.out.println(3);
+//            }
+//        }
+
+
+    }
+
+
+}
