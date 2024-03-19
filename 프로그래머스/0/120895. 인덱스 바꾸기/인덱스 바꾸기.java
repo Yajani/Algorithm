5f5c11ed1384be989cd2ee95cf7733a9cec181e5
예진초이 <115179558+Yajani@users.noreply.github.com>
2024-03-19 15:23:50 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 1.86 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -1,18 +1,6 @@
 class Solution {
     public String solution(String my_string, int num1, int num2) {
-        String[] str = my_string.split("");
         String answer = "";
-        
-        for(int i=0; i<my_string.length(); i++){
-            if(i == num1){
-                answer += str[num2];
-            }else if(i == num2){
-                answer += str[num1];
-            }else{
-                answer += str[i];
-            }
-        }
-        
         return answer;
     }
 }
