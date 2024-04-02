5846a653a522f58dd4d14f7df90af435f7b37ca4
예진초이 <115179558+Yajani@users.noreply.github.com>
2024-04-02 10:57:57 +0900

    [level 0] Title: 세로 읽기, Time: 0.10 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -5,6 +5,7 @@ class Solution {
         int startIndex = c - 1;
         while (startIndex < my_string.length()) {
             answer.append(my_string.charAt(startIndex));
+            //m만큼 더하면 startindex가 해당하는 다음열로 위치한다
             startIndex += m;
         }
 
