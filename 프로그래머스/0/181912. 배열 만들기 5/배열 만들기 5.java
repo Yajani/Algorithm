0c0df26beaa5c44e5a939e8c082a7544229f07f3
예진초이 <115179558+Yajani@users.noreply.github.com>
2024-05-08 15:21:18 +0900

    [level 0] Title: 배열 만들기 5, Time: 51.57 ms, Memory: 85.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String[] intStrs, int k, int s, int l) {
+        List<Integer> answer = new ArrayList<>();
+        
+        // 문자열 배열에서 문자에서 범위에 해당하는 문자열을 뽑아서 정수로 변환한뒤에 k 값이랑 비교해서 정수배열로 리턴한다
+        for(int i =0 ; i<intStrs.length; i++){
+            String str = "";
+            for(int j = s ; j < s+l; j++){
+               str += intStrs[i].charAt(j);
+            }
+            if(Integer.parseInt(str) > k){
+                answer.add(Integer.parseInt(str));
+            }
+        }
+        
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
