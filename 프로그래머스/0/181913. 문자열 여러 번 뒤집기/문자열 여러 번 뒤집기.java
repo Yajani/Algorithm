bf7e54dd7b04a1744060ec41200cf85d9e3e49d4
예진초이 <115179558+Yajani@users.noreply.github.com>
2024-09-13 00:15:20 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 2.56 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -1,36 +1,15 @@
-public class Solution {
-    public String solution(String s, int[][] queries) {
-        // 문자열을 문자 배열로 변환
-        char[] charArray = s.toCharArray();
-
-        // 각 쿼리에 대해 해당 구간을 뒤집는 작업을 수행
-        for (int[] query : queries) {
-            int start = query[0];
-            int end = query[1];
-            reverse(charArray, start, end);
+import java.lang.StringBuilder;
+class Solution {
+    public String solution(String my_string, int[][] queries) {
+        StringBuilder sb = new StringBuilder(my_string);
+        for (int i = 0; i < queries.length; i++) {
+            int start = queries[i][0];
+            int end = queries[i][1];
+            String substring = sb.substring(start, end + 1);
+            StringBuilder reversedSubstring = new StringBuilder(substring).reverse();
+            sb.replace(start, end + 1, reversedSubstring.toString());
         }
-
-        // 최종적으로 배열을 문자열로 변환
-        return new String(charArray);
-    }
-
-    // 특정 구간을 뒤집는 함수
-    private void reverse(char[] array, int start, int end) {
-        while (start < end) {
-            char temp = array[start];
-            array[start] = array[end];
-            array[end] = temp;
-            start++;
-            end--;
-        }
-    }
-
-    // 메인 함수 (테스트용)
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        String s = "abcdef";
-        int[][] queries = {{0, 2}, {2, 4}, {0, 5}};
-        
-        System.out.println(sol.solution(s, queries));  // 출력: "fedcba"
+        String answer = sb.toString();
+        return answer;
     }
 }
