28d0ed2dba5e789827ae15fc86a8a695acde1146
예진초이 <115179558+Yajani@users.noreply.github.com>
2025-08-04 13:43:19 +0900

    Time: 0 ms (100%), Space: 13.1 MB (60.76%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,17 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        n = len(nums)
-        for i in range(n):
-            for j in range(n):
-                if i != j and nums[i] + nums[j] == target:
-                    return [i, j]
+        memo = {}  # {숫자: 인덱스}
+        for i, v in enumerate(nums):
+            needed_number = target - v
+            if needed_number in memo:
+                return [memo[needed_number], i]
+            memo[v] = i
+
+
+# memo에 지금까지 본 숫자와 그 숫자의 인덱스를 저장함
+
+# 현재 숫자 v에 대해 target - v가 이미 memo에 있다면,
+
+# 그것과 현재 인덱스 i가 정답
+
+# 조건을 만족하는 두 숫자를 찾으면 바로 리턴
